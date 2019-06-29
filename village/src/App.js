62cb33b426aa1c5018120dc468266a564f9831d4
Matt Hagner <web@matthagner.com>
2019-06-29 12:09:55 -0500

    Add "/" Route for rendering the Smurfs component

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Route } from "react-router-dom";
 
 import "./App.css";
 import SmurfForm from "./components/SmurfForm";
@@ -30,7 +31,13 @@ class App extends Component {
     return (
       <div className="App">
         <SmurfForm addSmurf={this.addSmurf} />
-        <Smurfs smurfs={this.state.smurfs} />
+
+        <Route
+          path="/"
+          exact
+          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+        />
+        <Route />
       </div>
     );
   }
